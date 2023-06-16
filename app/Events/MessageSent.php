2b5f8bf7db5b9e8d91f99da370f9a875b78e79a4
HabibAroua <habib.aroua@sesame.com.tn>
2023-06-16 16:37:15 +0100

    optims the code

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -2,10 +2,7 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\{Channel, InteractsWithSockets, PresenceChannel, PrivateChannel};
 use Illuminate\Contracts\Broadcasting\{ShouldBroadcast, ShouldBroadcastNow};
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
